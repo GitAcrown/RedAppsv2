a7869151ce560674b289297f22b0af5be0e6d451
GitAcrown <Acrown@outlook.fr>
2021-03-08 18:06:01 +0100

    [ALPHA] Lancement de Brainfck

--- brainfck/brainfck.py
+++ brainfck/brainfck.py
@@ -486,27 +486,28 @@ class Brainfck(commands.Cog):
     async def brainfck_file_example(self, ctx):
         """Affiche un tuto pour créer votre fichier thème pour Brainfck"""
         txt = "Les fichiers thème sont des fichiers en **.yaml** qui suivent le format de cet exemple :\n```yaml\n" \
-              "# Paramètres obligatoires\n" \
+              "# ----- Paramètres obligatoires -----\n" \
               "id: QJ2021  # Identifiant de votre thème, 10 caractères max\n" \
               "name: Quiz Janvier 2021  # Nom de votre thème\n" \
               "description: Questions de culture générale tirées du Grand Quiz de l'Appart de Janvier 2021  # Courte description du thème\n" \
-              "author_id: 172376505354158080  # Votre ID\n" \
-              "# Paramètres Optionnels\n" \
+              "author_id: 172376505354158080  # Votre ID\n\n" \
+              "# ----- Paramètres Optionnels -----\n" \
               "custom_delay: 8  # Modifier le délai pour répondre aux questions (min. 5s), par défaut 10\n" \
               "thumbnail: https://i.imgur.com/JpNIjwm.png   # Ajouter une image pour représenter son thème\n" \
-              "color: ffa8be  # Couleur personnalisée du thème, en HEX sans prefixe\n" \
-              "# Contenu du thème\n" \
+              "color: ffa8be  # Couleur personnalisée du thème, en HEX sans prefixe\n\n" \
+              "# ----- Contenu du thème -----\n" \
               "content:\n" \
-              "| Quel pays a pour capitale Taipei ?:  # Question\n" \
-              "|| image: https://www.guidesulysse.com/images/destinations/iStock-861177234.jpg  # Optionnel: Image à afficher s'il y en a une\n" \
-              "|| good: Taïwan  # Bonne réponse\n" \
-              "|| bad:  # Mauvaises réponses : minimum 3 (sélectionnées aléatoirement lors des parties)\n" \
-              "||| - Corée du Nord\n" \
-              "||| - Japon\n" \
-              "||| - Corée du Sud\n" \
-              "||| - Vietnam\n" \
-              "|| show: >  # Optionnel: Message à ajouter après avoir répondu (pour donner des détails)\n" \
-              "||| Taipei est la capitale politique, culturelle et économique *de facto* de l'île de Taïwan.```"
+              ". Quel pays a pour capitale Taipei ?:  # Question\n" \
+              ".. image: https://www.guidesulysse.com/images/destinations/iStock-861177234.jpg  # Optionnel: Image à afficher s'il y en a une\n" \
+              ".. good: Taïwan  # Bonne réponse\n" \
+              ".. bad:  # Mauvaises réponses : min. 3\n" \
+              "... - Corée du Nord\n" \
+              "... - Japon\n" \
+              "... - Corée du Sud\n" \
+              "... - Vietnam\n" \
+              ".. show: >  # Optionnel: Message à ajouter après avoir répondu\n" \
+              "... Taipei est la capitale politique, culturelle et économique *de facto* de l'île de Taïwan.\n\n" \
+              ". = Indentation```"
 
         em = discord.Embed(title="Créer un fichier thème pour Brainfck", color=await ctx.embed_color(), description=txt)
         em.add_field(name="Notes importantes", value="- Les fichiers doivent contenir (dans 'content') au moins 15 questions valides pour être accepté\n"
