36c045cf8fecee01f01612189e9682256140764c
GitAcrown <Acrown@outlook.fr>
2021-03-14 22:12:38 +0100

    Ajout d'imgedit

--- imgedit/imgedit.py
+++ imgedit/imgedit.py
@@ -96,7 +96,7 @@ class ImgEdit(commands.Cog):
         watermark.thumbnail((round(width / proportion), round(height / proportion)))
         position = (width - watermark.size[0] - position[0], height - watermark.size[1] - position[1])
 
-        if base_image.format.lower() in ('gif', 'gifv'):
+        if base_image.filename.endswith('gif') or base_image.filename.endswith('gifv'):
             frames = []
             for frame in ImageSequence.Iterator(base_image):
                 transparent = Image.new('RGBA', (width, height), (0, 0, 0, 0))
