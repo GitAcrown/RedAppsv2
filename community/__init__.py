c1ce092edba328a445cf0bab77d047a20c4f2989
GitAcrown <Acrown@outlook.fr>
2021-06-02 15:42:26 +0200

    correction objet init

--- community/__init__.py
+++ community/__init__.py
@@ -3,4 +3,4 @@ from .community import Community
 __red_end_user_data_statement__ = 'This cog does not store personal data.'
 
 def setup(bot):
-    bot.add_cog(community(bot))
+    bot.add_cog(Community(bot))
